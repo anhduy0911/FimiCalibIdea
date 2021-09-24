4e0ffb9c00e1d0bd91799be71f022a7301f431b4
Duy <anhduy0911@gmail.com>
2021-09-24 14:03:12 +0700

    add 2 dataset

--- aqm_global.sh
+++ aqm_global.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 forgan.py -ds aqm -t lstm -steps 5000 -n 4 -c 31 -rg 64 -rd 256 -d_iter 6 -hbin 50 -type train -metric rmse -best True
+python3 forgan_multistep.py -ds aqm -t lstm -steps 5000 -n 3 -c 7 -p 3 -rg 32 -rd 128 -d_iter 6 -hbin 50 -type train -best=True -metric rmse -raw True -mul True
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,42 +1,102 @@
+import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 
 def plot_data():
-    import matplotlib.dates as mdates
-
-    aqm1 = pd.read_csv("Data/fimi/fimi_13.csv", header=0)
-    aqm3 = pd.read_csv("Data/fimi/fimi_12.csv", header=0)
-    print(aqm1.describe())
+    aqm1 = pd.read_csv("Data/aqmesh1.csv", header=0)[26:1458]
+    aqm2 = pd.read_csv("Data/aqmesh2.csv", header=0)[25:1650]
+    aqm3 = pd.read_csv("Data/aqmesh3.csv", header=0)[26:1659]
+    fimi2 = pd.read_csv("Data/fimi2.csv", header=0)[13169:17572:3]
+    print(aqm2.describe())
     print(aqm3.describe())
 
-    aqm1_part = pd.DataFrame()
-    aqm1_part['PM2_5'] = aqm1['PM2_5'][:17012]
-    aqm1_part['temp'] = aqm1['temperature'][:17012]
-    aqm1_part['humidity'] = aqm1['humidity'][:17012]
-    # print(aqm1_part.describe())
-    # aqm3_part = pd.DataFrame()
-    aqm1_part['PM2_5_cal'] = aqm3['PM2_5']
-    aqm1_part['temp_cal']= aqm3['temperature']
-    aqm1_part['humidity_cal'] = aqm3['humidity']
-    aqm1_part.to_csv('data/fimi/12_13.csv', header=True, index=False)
-    # print(aqm3_part.describe())
-    # aqm3_part.to_csv('Data/aqmes3_part.csv', header=True, index=False)
-    fig, (ax1, ax2, ax3) = plt.subplots(3,1, sharex=True)
-    ax1.plot(range(len(aqm1['PM2_5'])),aqm1['PM2_5'])
-    ax1.plot(range(len(aqm3['PM2_5'])),aqm3['PM2_5'])
-    ax1.set(xlabel='Date', ylabel='PM2.5')
-    ax2.plot(range(len(aqm1['PM2_5'])),aqm1['temperature'])
-    ax2.plot(range(len(aqm3['PM2_5'])),aqm3['temperature'])
-    ax2.set(xlabel='Date', ylabel='temp')
-    ax3.plot(range(len(aqm1['PM2_5'])),aqm1['humidity'])
-    ax3.plot(range(len(aqm3['PM2_5'])),aqm3['humidity'])
-    ax3.set(xlabel='Date', ylabel='humidity')
+    aqm2['datetime'] = pd.to_datetime(aqm2['datetime'], format='%Y-%m-%d %H:%M:%S') 
+    aqm3['datetime'] = pd.to_datetime(aqm3['datetime'], format='%Y-%m-%d %H:%M:%S')
+    fimi2['datetime'] = pd.to_datetime(fimi2['datetime'], format='%m/%d/%Y %H:%M').dt.round('5min')
+
+    # print(fimi2['datetime'].head())
+    # print(aqm2['datetime'].head())
+    # print(aqm3['datetime'].head())
+
+    merged = pd.merge(aqm3, fimi2, how='outer', on='datetime')
+    clean_dat = pd.DataFrame()
+    clean_dat['datetime'] = merged['datetime']
+    clean_dat['PM2_5'] = merged['PM2_5_y']
+    clean_dat['temp'] = merged['temp_y']
+    clean_dat['humidity'] = merged['humidity_y']
+    clean_dat['PM2_5_cal'] = merged['PM2_5_x']
+    clean_dat['temp_cal'] = merged['temp_x']
+    clean_dat['humidity_cal'] = merged['humidity_x']    
+
+    clean_dat = clean_dat.dropna(axis=0)
+    clean_dat.to_csv('Data/aqmesh_fimi.csv', index=None)
+    print(clean_dat.head(10))
     
+
+    #plot
+
+    fig, (ax1) = plt.subplots(1,1, sharex=True)
+    ax1.plot(merged['datetime'],merged['PM2_5_x'], 'b')
+    ax1.plot(merged['datetime'],merged['PM2_5_y'], 'r')
+    # ax1.set(xlabel='Date', ylabel='PM2.5')
+    # # ax2.plot(range(len(aqm2['PM2_5'])),aqm2['temp'], 'b-')
+    # # ax2.plot(range(len(aqm3['PM2_5'])),aqm3['temp'], 'g-')
+    # # ax2.plot(range(len(fimi2['PM2_5'])),fimi2['temp'], 'r-')
+    # # ax2.set(xlabel='Date', ylabel='temp')
+    # # ax3.plot(range(len(aqm2['PM2_5'])),aqm2['humidity'], 'b-')
+    # # ax3.plot(range(len(aqm3['PM2_5'])),aqm3['humidity'], 'g-')
+    # # ax3.plot(range(len(fimi2['PM2_5'])),fimi2['humidity'], 'r-')
+    # # ax3.set(xlabel='Date', ylabel='humidity')
+    
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
+def plot_data_grim():
+    grim3 = pd.read_csv("Data/grim3.csv", header=0)
+    grim7 = pd.read_csv("Data/grim7.csv", header=0)
+    fimi2 = pd.read_csv("Data/fimi2.csv", header=0)
+
+    grim3['datetime'] = pd.to_datetime(grim3['datetime'], format='%Y-%m-%d %H:%M:%S')
+    grim7['datetime'] = pd.to_datetime(grim7['datetime'], format='%Y-%m-%d %H:%M:%S').dt.round('5min')
+    fimi2['datetime'] = pd.to_datetime(fimi2['datetime'], format='%m/%d/%Y %H:%M').dt.round('5min')
+    print(grim7['datetime'].head())
+    print(fimi2['datetime'].head())
+
+    merged = pd.merge(grim7, fimi2, how='outer', on='datetime')
+    clean_dat = pd.DataFrame()
+    clean_dat['datetime'] = merged['datetime']
+    clean_dat['PM2_5'] = merged['PM2_5_y']
+    clean_dat['temp'] = merged['temp']
+    clean_dat['humidity'] = merged['humidity']
+    clean_dat['PM2_5_cal'] = merged['PM2_5_x']
+    # clean_dat['temp_cal'] = merged['temp_x']
+    # clean_dat['humidity_cal'] = merged['humidity_x']    
+
+    clean_dat = clean_dat.dropna(axis=0)
+    clean_dat.to_csv('Data/grim_fimi.csv', index=None)
+    print(clean_dat.head(10))
+    #plot
+
+    fig, (ax1) = plt.subplots(1,1, sharex=True)
+    ax1.plot(clean_dat['datetime'],clean_dat['PM2_5'], 'g-')
+    ax1.plot(clean_dat['datetime'],clean_dat['PM2_5_cal'], 'r-')
+    # ax1.set(xlabel='Date', ylabel='PM2.5')
+    # ax2.plot(range(len(aqm2['PM2_5'])),aqm2['temp'], 'b-')
+    # ax2.plot(range(len(aqm3['PM2_5'])),aqm3['temp'], 'g-')
+    # ax2.plot(range(len(fimi2['PM2_5'])),fimi2['temp'], 'r-')
+    # ax2.set(xlabel='Date', ylabel='temp')
+    # ax3.plot(range(len(aqm2['PM2_5'])),aqm2['humidity'], 'b-')
+    # ax3.plot(range(len(aqm3['PM2_5'])),aqm3['humidity'], 'g-')
+    # ax3.plot(range(len(fimi2['PM2_5'])),fimi2['humidity'], 'r-')
+    # ax3.set(xlabel='Date', ylabel='humidity')
+    
+    plt.gcf().autofmt_xdate()
     plt.show()
 
 
 if __name__ == '__main__':
     plot_data()
+    # plot_data_grim()
     # import numpy as np
     # aqm = pd.read_csv("Data/aqmes1_part.csv", header=0)
     # index = aqm['PM2_5'].index[aqm['PM2_5'].apply(np.isnan)]
