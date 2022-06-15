01331866d83b69eae1e557990fc1d31ae0efc8d6
Hoang Dang Nguyen <dang.nh194423@sis.hust.edu.vn>
2022-06-15 15:17:37 +0700

    update readme

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ components.py
 You can take the components, as well as models from the base file of multicalib model:
 ```
 #run training model with default settings:
-python main.py
+python main.py --usen 2
 
 #training loop for multicalib model:
 multicalib.py
@@ -28,6 +28,15 @@ multicalib.py
 #modules for the model:
 models/modules.py
 ```
+The same set for N model setting and 1 model setting
+```
+#for N models for N tasks
+python main.py --usen 0 
+
+#for 1 model for N tasks
+python main.py --usen 1
+```
+
 ## Environment
 - Python
 - Pytorch
--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import argparse
 from single_calib import SingleCalibModel
 import utils
 # from forgan import ForGAN
-# from multicalib_Nmodel import MultiCalibModel
-from multicalib import MultiCalibModel
 # from multicalib import MultiCalibModel
 import config as CFG
 
@@ -33,8 +31,8 @@ if __name__ == '__main__':
                     help="train")
     ap.add_argument("--name", metavar='', dest="name", type=str, default='multi',
                     help="name of the model - project")
-    ap.add_argument("--usen", metavar='', dest="use_n", type=bool, default=False,
-                    help="whether to use n model or only one")
+    ap.add_argument("--usen", metavar='', dest="use_n", type=int, default=0,
+                    help="whether to use n model (0) or only one (1) or propose model (2)")
     opt = ap.parse_args()
 
     # x_train, y_train, lab_train, x_val, y_val, lab_val, x_test, y_test, lab_test = utils.prepare_multicalib_dataset()
@@ -55,8 +53,17 @@ if __name__ == '__main__':
     seed_everything(911)
     opt.device_ids = CFG.devices
     opt.attributes = CFG.attributes
-    model = MultiCalibModel(opt, x_train, y_train, lab_train, x_val, y_val, lab_val, x_test, y_test, lab_test, use_n=opt.use_n)
-    # model = SingleCalibModel(opt, x_train, y_train, lab_train, x_val, y_val, lab_val, x_test, y_test, lab_test)
+
+    if opt.use_n == 0:
+        from multicalib_Nmodel import MultiCalibModel
+        model = MultiCalibModel(opt, x_train, y_train, lab_train, x_val, y_val, lab_val, x_test, y_test, lab_test, use_n=True)
+    if opt.use_n == 1:
+        from multicalib_Nmodel import MultiCalibModel
+        model = MultiCalibModel(opt, x_train, y_train, lab_train, x_val, y_val, lab_val, x_test, y_test, lab_test, use_n=False)
+    else:
+        from multicalib import MultiCalibModel
+        model = MultiCalibModel(opt, x_train, y_train, lab_train, x_val, y_val, lab_val, x_test, y_test, lab_test)
+    
     if opt.train_type == 'train':
         model.train()
         model.test()
